a3b567df8a2f03165018cd501693b4cab5d2d82a
Banff International Research Station <brentk@birs.ca>
2022-01-07 16:34:25 -0700

    add globe.png to manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,7 @@
 //= link_directory ../javascripts
 //= link_directory ../stylesheets
 //= link_tree ../images
+//= link globe.png
 //= link application.css
 //= link administrate/application.css
 //= link administrate/application.js
