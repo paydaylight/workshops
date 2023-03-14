97ba0c091e9b1bae97ee6d6056b3e2b4a997396a
paydaylight <maxat235@gmail.com>
2023-03-14 19:13:34 +0600

    Fix method

--- app/mailers/event_statistics_mailer.rb
+++ app/mailers/event_statistics_mailer.rb
@@ -7,7 +7,7 @@ class EventStatisticsMailer < ApplicationMailer
     @confirmed_count = Membership.confirmed.where(event: @event).count
     @invited_count = Membership.invited.where(event: @event).count
     @undecided_count = Membership.undecided.where(event: @event).count
-    @physical_spots = @event.max_participants - @event.num_invited_participants
+    @physical_spots = @event.max_participants - @event.num_invited_in_person
     @virtual_spots = @event.max_virtual - @event.num_invited_virtual
     recipients = []
 
