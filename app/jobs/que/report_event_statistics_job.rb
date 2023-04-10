9e2c2bc570958c799177762578778e3c661c54ac
paydaylight <maxat235@gmail.com>
2023-04-10 19:49:51 +0600

    Update ReportEventStatisticsJob next run at time

--- app/jobs/que/report_event_statistics_job.rb
+++ app/jobs/que/report_event_statistics_job.rb
@@ -19,10 +19,10 @@ module Que
     end
 
     def next_run_at(event)
-      if ::Rails.env.production? || ::Rails.env.test?
-        production_run_at(event)
-      else
+      if ::Rails.env.development?
         development_run_at(event)
+      else
+        production_run_at(event)
       end
     end
 
